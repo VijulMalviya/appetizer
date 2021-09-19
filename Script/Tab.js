1b5a054f1e3c61c89ca92a19dd38334844cc5ac8
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-19 23:34:49 +0530

    Add files via upload

---
+++ Script/Tab.js
@@ -0,0 +1,14 @@
+function FoodCat(evt, FoodType) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(FoodType).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  document.getElementById("defaultOpen").click();
--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,10 @@
 AOS.init();
 
+$(window).on('load',function(){
+  $(".loader").fadeOut("2000");
+
+})
+
 $(document).ready(function(){
     $("#bars").click(function(){
       $(".nav-links").slideToggle("slow");
@@ -7,13 +12,19 @@ $(document).ready(function(){
   });
 
 
-  window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementsByClassName(".Navigation").style.backgroundColor = "white";
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    $('.Navigation').css('background-color','white');
+    $('.Navigation').addClass('shadow');
+    $('.Navigation a').css('color','black');
   } else {
-    document.getElementsByClassName(".Navigation").style.backgroundColor = "transparent";
-
+    $('.Navigation').css('background-color','transparent');
+    $('.Navigation').removeClass('shadow');
+    $('.Navigation a').css('color','white');
   }
 }
+
+
+
