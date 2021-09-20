7fb69d2f7bf735b06ba1a16a306c58c8c1df576e
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-09-20 11:29:35 +0530

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -7,11 +7,25 @@ $(window).on('load',function(){
 
 $(document).ready(function(){
     $("#bars").click(function(){
-      $(".nav-links").slideToggle("slow");
+      
+
+      if ($('.Navigation i').hasClass("fa-bars")) {
+        $(".Navigation i").removeClass("fa-bars");
+        $(".Navigation i").addClass("fa-times");
+        $(".nav-links").slideDown("slow");
+    
+      }
+      // Else, the element doesn't have the active class, so we remove it from every element before applying it to the element that was clicked
+      else {
+        $(".Navigation i").addClass("fa-bars");
+        $(".Navigation i").removeClass("fa-times");
+        $(".nav-links").slideUp("slow");
+      }
     });
   });
 
 
+  
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
